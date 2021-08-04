5cfcfc4b8da257f3013f8a1dd912d59500730c5c
Owen Lloyd <33433568+owen7lloyd@users.noreply.github.com>
2021-08-04 11:16:24 -0400

    finished vectorized inputs

--- dagapp/examples/vectorized_example.py
+++ dagapp/examples/vectorized_example.py
@@ -6,22 +6,20 @@ from functools import partial
 from dagapp.page_funcs import VectorizePageFunc
 
 
-def b(a):
+def b(a: int):
     return 2 ** a
 
 
-def d(c):
+def d(c: int):
     return 10 - (5 ** c)
 
 
-def result(b, d):
+def result(b: int, d: int):
     return b * d
 
 
-dags = [
-    DAG((b, d, result)),
-]
+dags = [DAG([b, d, result])]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = partial(dag_app, dags=dags, page_factory=VectorizePageFunc)
     app()
